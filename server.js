7a0b5ca4284b35bd89d90abf5a30aae701360df8
HBK <sammcowen@gmail.com>
2022-03-25 13:23:32 -0700

    adding to file structure. creating server.js

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const exphbs = require('express-handlebars');
 const opath = require('path');
 const session = require('express-session');
 const { urlencoded } = require('express');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
 // const sequelize = require()
 // setting up handlebars as the view engine for views folder
-app.engine('view engine', 'handelbars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // middleware
 app.use(express.json());
 app.use(urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 // setting up server to be synced with sequelize db
 sequelize.sync({force:false}).then(() => {
     app.listen(PORT, () => console.log(`Now listening on Port ${PORT}`));
