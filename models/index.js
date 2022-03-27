2e8e60debd99340107281754fc5998ea8c338f54
HBK <sammcowen@gmail.com>
2022-03-27 15:17:49 -0700

    created COmment Model and updated association in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,27 @@
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
 // associations 
 User.hasMany(Post, {
     foreignKey:'user_id'
 });
+User.hasMany(Comment,{
+    foreignKey:'user_id'
+})
 Post.belongsTo(User,{
+    foreignKey:'user_id'
+});
+Post.hasMany(Comment, {
+    foreignKey:'post_id'
+});
+Comment.belongsTo(User, {
+    foreign_key:"user_id"
+});
+Comment.belongsTo(Post,{
     foreignKey:'user_id'
 });
 
 
-module.exports = {User, Post}
+
+module.exports = {User, Post, Comment}
